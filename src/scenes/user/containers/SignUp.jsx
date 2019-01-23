e00f34e49827c0d6f467f89f238bfbabe4f79cc6
Ralf Junghanns <ralf.junghanns@gmail.com>
2019-01-23 17:32:10 +0100

    Remove env-variable REACT_APP_BASE_URL

--- src/scenes/user/containers/SignUp.jsx
+++ src/scenes/user/containers/SignUp.jsx
@@ -202,7 +202,13 @@ class SignUp extends React.Component {
     onSignUpClick = () => {
         const formValidation = this.validateForm();
         if (formValidation.isValid) {
-            const redirectTo = process.env.REACT_APP_BASE_URL + '/login';
+
+            let redirectTo = '';
+            if (typeof window !== 'undefined') {
+                // eslint-disable-next-line no-restricted-globals
+                redirectTo = location.protocol + '//' + location.host + '/login';
+            }
+
             this.signUpRequest(this.state.name, this.state.username, this.state.email, this.state.password, redirectTo);
             this.setState({loading: true});
         }
