dd00d3042fad4250241a6feb7fda021793fafc96
Ralf Junghanns <ralf.junghanns@gmail.com>
2019-01-23 21:22:25 +0100

    Make tests run

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import routes from './routes'
 
+import configureStore from './store';
+import {Provider} from 'react-redux';
+
+const store = configureStore();
+
 const App = () => (
-    <BrowserRouter>
-        {routes}
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            {routes}
+        </BrowserRouter>
+    </Provider>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {Provider} from 'react-redux'
-import configureStore from './store';
+
 import './semantic/dist/semantic.min.css';
 
-const store = configureStore();
-
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
+    <App/>,
     document.getElementById('root')
 );
 
