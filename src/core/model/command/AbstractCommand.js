fa142233698001438689c947e079ead1b7f6aade
Ralf Junghanns <ralf.junghanns@gmail.com>
2019-01-28 16:56:19 +0100

    Refactoring JSON-Schema validation

--- src/core/model/command/AbstractCommand.js
+++ src/core/model/command/AbstractCommand.js
@@ -1,52 +1,64 @@
 import Ajv from 'ajv';
 import ajv0 from 'ajv/lib/refs/json-schema-draft-04';
+import jsrp from 'json-schema-ref-parser'
 import uuid from 'uuid';
 
 
 export default class AbstractCommand {
 
-    metadata = [];
+    metadata = {};
     uuid = uuid();
 
     constructor(name, payload, schema = null) {
         this.message_name = name;
         this.payload = payload;
         this.schema = schema;
-
-        const [isValid, errors] = this.validate();
-        if (!isValid) {
-            console.warn(
-                'Invalid payload sending ' + this.message_name,
-                JSON.stringify(errors)
-            );
-        }
+        this.validate();
     }
 
     validate() {
-        const ajv = new Ajv({schemaId: 'auto'});
+        return new Promise((resolve) => {
+            if (!this.schema) {
+                const message = 'No schema given for validation';
+                console.warn(message);
+                resolve([false, message]);
+                return;
+            }
 
-        if (!this.schema) {
-            const message = 'No schema given for validation';
-            console.warn(message);
-            return [false, message];
-        }
+            const ajv = new Ajv({schemaId: 'auto'});
 
-        ajv.addMetaSchema(ajv0);
-        const val = ajv.compile(this.schema);
-        const isValid = val(this.payload);
-        const errors = val.errors;
+            // THE WAY WITH URLs
+            if (this.schema.indexOf('https://') === 0) {
+                jsrp.dereference({'$ref': this.schema})
+                    .then(schema => {
+                        const val = ajv.compile(schema);
+                        const isValid = val(this.toObject());
+                        const errors = val.errors;
 
-        if (!isValid) {
-            console.warn('Invalid payload sending ' + this.message_name, JSON.stringify(errors));
-        }
+                        if (!isValid) {
+                            console.warn('Invalid payload sending ' + this.message_name, this.toObject(), this.schema, JSON.stringify(errors));
+                        }
 
-        return [isValid, errors];
+                        resolve([isValid, errors]);
+                    })
+                    .catch(e => resolve([false, e]));
+                return;
+            }
+
+            // THE OLD WAY
+            ajv.addMetaSchema(ajv0);
+            const val = ajv.compile(this.schema);
+            const isValid = val(this.payload);
+            const errors = val.errors;
+
+            if (!isValid) {
+                console.warn('Invalid payload sending ' + this.message_name, JSON.stringify(errors));
+            }
+            resolve([isValid, errors]);
+        });
+
+
     }
-
-    getPayload = () => {
-        this.validate();
-        return this.toObject()
-    };
 
     toObject = () => ({
         uuid: this.uuid,
