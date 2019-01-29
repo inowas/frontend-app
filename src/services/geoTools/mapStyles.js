004e6aedaf2544b8ea1419c6431e295633e1351e
andreea <andreea.stefan.de@gmail.com>
2019-01-29 13:59:36 +0100

    T03 adjust shape colors and lines in maps

--- src/services/geoTools/mapStyles.js
+++ src/services/geoTools/mapStyles.js
@@ -1,15 +1,16 @@
 const styles = {
     active_cells: {
         line: {
-            color: 'grey',
-            weight: 0.3
+            color: '#4C4C4C',
+            weight: 0.2
         }
     },
     area: {
         weight: 2,
         opacity: 0.7,
-        color: '#1EB1ED',
-        fill: false
+        color: '#1D7FED',
+        fill: '#1D7FED',
+        dashArray: '5,3'
     },
     bounding_box: {
         color: '#000',
@@ -19,7 +20,7 @@ const styles = {
     grid: {
         weight: 1,
         opacity: 1,
-        color: 'blue',
+        color: '#1D7FED',
         fill: false
     },
     chd: {
@@ -38,110 +39,110 @@ const styles = {
         radius: 3,
         color: 'green',
         weight: 1,
-        fillColor: 'yellow',
+        fillColor: '#FBBD08',
         fillOpacity: 1
     },
     rch: {
         weight: 2,
-        color: '#ED8D05',
+        color: '#95549f',
         opacity: 0.7,
         fillOpacity: 0.3
     },
     riv: {
         weight: 5,
-        color: '#ED8D05',
+        color: '#1D7FED',
         opacity: 1
     },
     riv_op: {
         radius: 5,
-        color: '#404040',
+        color: '#4C4C4C',
         weight: 2,
-        fillColor: '#404040',
+        fillColor: '#1EB1ED',
         fillOpacity: 0.7
     },
     wel: {
         cw: {
-            radius: 3,
+            radius: 4,
             color: 'black',
-            weight: 1,
-            fillColor: 'darkgreen',
+            weight: 2,
+            fillColor: '#4CAF53',
             fillOpacity: 0.7
         },
         puw: {
-            radius: 3,
+            radius: 4,
             color: 'black',
-            weight: 1,
-            fillColor: 'darkblue',
+            weight: 2,
+            fillColor: '#0288D1',
             fillOpacity: 0.7
         },
         iw: {
-            radius: 3,
+            radius: 4,
             color: 'black',
-            weight: 1,
-            fillColor: 'blue',
+            weight: 2,
+            fillColor: '#1D7FED',
             fillOpacity: 0.7
         },
         inw: {
             radius: 4,
             color: 'black',
-            weight: 1,
-            fillColor: 'yellow',
+            weight: 2,
+            fillColor: '#FBBD08',
             fillOpacity: 0.7
         },
         irw: {
-            radius: 3,
+            radius: 4,
             color: 'black',
-            weight: 1,
-            fillColor: 'darkgreen',
+            weight: 2,
+            fillColor: '#4CAF53',
             fillOpacity: 0.7
         },
         opw: {
-            radius: 3,
-            color: 'red',
-            weight: 1,
-            fillColor: 'red',
+            radius: 4,
+            color: '#D50E00',
+            weight: 2,
+            fillColor: '#FF5B4D',
             fillOpacity: 0.7
         },
         sniw: {
             radius: 5,
             color: 'red',
             weight: 2,
-            fillColor: 'darkgreen',
+            fillColor: '#4CAF53',
             fillOpacity: 0.7
         },
         snpw: {
             radius: 5,
             color: 'red',
             weight: 2,
-            fillColor: 'darkblue',
+            fillColor: '#0288D1',
             fillOpacity: 0.7
         },
         prw: {
             radius: 3,
             color: 'black',
             weight: 1,
-            fillColor: 'darkblue',
+            fillColor: '#0288D1',
             fillOpacity: 0.7
         },
         rbf: {
             radius: 5,
             color: 'black',
             weight: 1,
-            fillColor: 'yellow',
+            fillColor: '#FBBD08',
             fillOpacity: 1
         },
         smw: {
             radius: 5,
             color: 'black',
             weight: 1,
-            fillColor: 'red',
+            fillColor: '#FF5B4D',
             fillOpacity: 1
         },
         snw: {
             radius: 5,
             color: 'black',
             weight: 1,
-            fillColor: 'yellow',
+            fillColor: '#FBBD08',
             fillOpacity: 1
         },
         snifw: {
@@ -152,21 +153,21 @@ const styles = {
             fillOpacity: 0.7
         },
         activeWell: {
-            fillColor: 'yellow'
+            fillColor: '#FBBD08'
         }
     },
     op: {
         radius: 5,
-        color: '#404040',
+        color: '#4C4C4C',
         weight: 2,
-        fillColor: '#404040',
+        fillColor: '#4C4C4C',
         fillOpacity: 1
     },
     op_temp: {
         radius: 5,
-        color: '#404040',
-        weight: 1,
-        fillColor: '#404040',
+        color: '#4C4C4C',
+        weight: 2,
+        fillColor: '#4C4C4C',
         fillOpacity: 0.5
     },
     op_selected: {
@@ -180,9 +181,9 @@ const styles = {
         radius: 5,
         weight: 2,
         opacity: 1,
-        color: 'blue',
+        color: '#1D7FED',
         dashArray: '1',
-        fillColor: 'blue',
+        fillColor: '#1D7FED',
         fillOpacity: 0.7
     }
 };
