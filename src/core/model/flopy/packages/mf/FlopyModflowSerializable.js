274687c1f5c2574d6053b57cdea784a472294ee0
Ralf Junghanns <ralf.junghanns@gmail.com>
2019-03-01 23:48:37 +0100

    Add sort function

--- src/core/model/flopy/packages/mf/FlopyModflowSerializable.js
+++ src/core/model/flopy/packages/mf/FlopyModflowSerializable.js
@@ -1,3 +1,5 @@
+import md5 from 'md5';
+
 export default class FlopyModflowSerializable {
 
     static fromObject(obj) {
@@ -19,5 +21,35 @@ export default class FlopyModflowSerializable {
         }
 
         return obj;
+    }
+
+    sort = (object) => {
+        const sortedObj = {};
+        const keys = Object.keys(object);
+
+        keys.sort((key1, key2) => {
+            key1 = key1.toLowerCase();
+            key2 = key2.toLowerCase();
+
+            if (key1 < key2) return -1;
+            if (key1 > key2) return 1;
+            return 0;
+        });
+
+        for (let index in keys) {
+            const key = keys[index];
+            if (typeof object[key] == 'object' && !(object[key] instanceof Array) && !(object[key] === null)) {
+                sortedObj[key] = this.sort(object[key]);
+            } else {
+                sortedObj[key] = object[key];
+            }
+        }
+
+        return sortedObj;
+    };
+
+    hash = () => {
+        const sorted = this.sort(this.packages);
+        return md5(JSON.stringify(sorted));
     }
 }
--- src/test/core/model/flopy/packages/FlopyModflowMf.test.js
+++ src/test/core/model/flopy/packages/FlopyModflowMf.test.js
@@ -1,4 +1,5 @@
 import {FlopyModflowMf} from 'core/model/flopy/packages/mf';
+import FlopyModflowMfbas from '../../../../../core/model/flopy/packages/mf/FlopyModflowMfbas';
 
 
 test('It can instantiate FlopyModflowMf', () => {
@@ -9,4 +10,14 @@ test('It can instantiate FlopyModflowMf', () => {
     const modelname = 'newModelName' + Math.random();
     modflowMf.modelname = modelname;
     expect(modflowMf.modelname).toEqual(modelname);
+});
+
+test('It generates a hash', () => {
+    const model = new FlopyModflowMf();
+    FlopyModflowMfbas.createWithModel(model);
+    expect(model.hash()).toEqual('47fbf6e70805ca5df921b29ee62f9a7c');
+    model.modelname = 'modflowtest2';
+    expect(model.hash()).toEqual('89c20f1b89d6ba6ea8912467a7fff493');
+    model.modelname = 'modflowtest';
+    expect(model.hash()).toEqual('47fbf6e70805ca5df921b29ee62f9a7c');
 });
