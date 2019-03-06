63278faea896f35ee0523a61126d6dc139db7c2c
andreea <andreea.stefan.de@gmail.com>
2019-03-06 11:11:38 +0100

    Adjust layout of InfoT13A and InfoT09A

--- src/scenes/t13/components/infoT13A.jsx
+++ src/scenes/t13/components/infoT13A.jsx
@@ -3,29 +3,19 @@ import {pure} from 'recompose';
 import PropTypes from 'prop-types';
 import {calculateTravelTimeT13A} from '../calculations/calculationT13A';
 import {getParameterValues} from '../../shared/simpleTools/helpers';
-import {Grid, Header} from 'semantic-ui-react';
-
-const style = {
-    text: {
-        padding: '0 20px'
-    }
-};
+import {Icon, Message} from 'semantic-ui-react';
 
 const Info = ({parameters}) => {
     const {W, K, ne, L, hL, xi, xe} = getParameterValues(parameters);
     const t = calculateTravelTimeT13A(xe, W, K, ne, L, hL, xi);
     return (
-        <Grid>
-            <Grid.Row centered>
-                <Header as='h2'>Info</Header>
-            </Grid.Row>
-            <Grid.Row centered>
-                <p style={style.text}>
-                    The travel time between initial position and arrival location
-                    is <strong>{t.toFixed(1)} days</strong>.
-                </p>
-            </Grid.Row>
-        </Grid>
+        <Message icon info>
+            <Icon name='info circle' color='blue' />
+            <p>
+                The travel time between initial position and arrival location
+                is <strong>{t.toFixed(1)} days</strong>.
+            </p>
+        </Message>
     );
 };
 
