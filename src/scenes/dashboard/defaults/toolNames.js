847e24f4ff78ffaceb50b9505ce693fe47a795f9
Ralf Junghanns <ralf.junghanns@gmail.com>
2019-03-11 13:41:39 +0100

    Update ToolMetaData adding new file mapping toolnames

---
+++ src/scenes/dashboard/defaults/toolNames.js
@@ -0,0 +1,59 @@
+const toolNames = [
+    {
+        slug: 'T02',
+        name: 'Groundwater mounding (Hantush)',
+    }, {
+        slug: 'T03',
+        name: 'MODFLOW model setup and editor',
+    }, {
+        slug: 'T04',
+        name: 'Database for GIS-based suitability mapping',
+    }, {
+        slug: 'T05',
+        name: 'GIS multi-criteria decision analysis',
+    }, {
+        slug: 'T06',
+        name: 'MAR method selection',
+    }, {
+        slug: 'T07',
+        name: 'MODFLOW model scenario manager',
+    }, {
+        slug: 'T08',
+        name: '1D transport model (Ogata-Banks)',
+    }, {
+        slug: 'T09A',
+        name: 'Simple saltwater intrusion equations',
+    }, {
+        slug: 'T09B',
+        name: 'Simple saltwater intrusion equations',
+    }, {
+        slug: 'T09C',
+        name: 'Simple saltwater intrusion equations',
+    }, {
+        slug: 'T09D',
+        name: 'Simple saltwater intrusion equations',
+    }, {
+        slug: 'T09E',
+        name: 'Simple saltwater intrusion equations',
+    }, {
+        slug: 'T09F',
+        name: 'Simple saltwater intrusion equations',
+    }, {
+        slug: 'T11',
+        name: 'MAR model selection',
+    }, {
+        slug: 'T13',
+        name: 'Travel time through unconfined aquifer',
+    }, {
+        slug: 'T14',
+        name: 'Pumping-induced river drawdown',
+    }, {
+        slug: 'T17',
+        name: 'Global MAR portal',
+    }, {
+        slug: 'T18',
+        name: 'SAT basin design',
+    }
+];
+
+export default toolNames;
