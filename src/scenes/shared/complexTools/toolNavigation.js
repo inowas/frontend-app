a3fa20fe34557b379bb8816b2c33594167bb2853
andreea <andreea.stefan.de@gmail.com>
2019-03-12 13:54:58 +0100

    Tool Navigation T03: group menu items, provide headers for item groups

--- src/scenes/shared/complexTools/toolNavigation.js
+++ src/scenes/shared/complexTools/toolNavigation.js
@@ -3,6 +3,23 @@ import PropTypes from 'prop-types';
 import {withRouter} from 'react-router-dom';
 import {Menu, Segment} from 'semantic-ui-react';
 
+const styles = {
+    customMenuContainer: {
+        paddingTop: '0.5em',
+        paddingBottom: '0.5em'
+    },
+    customVertMenu: {
+        boxShadow: 'none'
+    },
+    menuItemGroup: {
+        paddingLeft: '0.5em',
+        fontSize: '1.07145em'
+    },
+    menuItemHeader: {
+        textTransform: 'uppercase'
+    }
+};
+
 class ToolNavigation extends React.Component {
 
     handleItemClick = (e, {route}) => {
@@ -16,22 +33,32 @@ class ToolNavigation extends React.Component {
         const basePath = path.split(':')[0];
         const {navigationItems} = this.props;
 
-        const menuItems = navigationItems.map((i, idx) => (
-            <Menu.Item
-                disabled={!id || i.disabled}
-                key={idx}
-                name={i.name}
-                active={property === i.property}
-                route={basePath + id + '/' + i.property}
-                onClick={this.handleItemClick}
-                >
-                {i.icon}{i.name}
-            </Menu.Item>
-        ));
+        const menuItems = navigationItems.map((itemGroup, itemGroupIdx) => (
+
+                <Menu.Item key={itemGroupIdx} style={styles.menuItemGroup}>
+                    <Menu.Header as='h4' style={styles.menuItemHeader}>{itemGroup.header}</Menu.Header>
+                    <Menu.Menu>
+                        {itemGroup.items.map((i, idx) => (
+                            <Menu.Item
+                                disabled={!id || i.disabled}
+                                key={idx}
+                                name={i.name}
+                                active={property === i.property}
+                                route={basePath + id + '/' + i.property}
+                                onClick={this.handleItemClick}
+                            >
+                                {i.icon}{i.name}
+                            </Menu.Item>
+                        ))}
+                    </Menu.Menu>
+                </Menu.Item>
+            ));
+
+
         return (
-            <Segment color={'blue'} style={this.props.style}>
-                <Menu secondary vertical style={{width: '100%'}}>
-                    {menuItems}
+            <Segment color={'blue'} style={styles.customMenuContainer}>
+                <Menu vertical fluid style={styles.customVertMenu}>
+                        {menuItems}
                 </Menu>
             </Segment>
         )
