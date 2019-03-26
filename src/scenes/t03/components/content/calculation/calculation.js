1757c73396a1249bc6d0acf1e1bb68a2b6c3f9d5
Ralf Junghanns <ralf.junghanns@gmail.com>
2019-03-26 22:55:09 +0100

    Fixes

--- src/scenes/t03/components/content/calculation/calculation.js
+++ src/scenes/t03/components/content/calculation/calculation.js
@@ -6,11 +6,8 @@ import Files from './files';
 
 const menuItems = [
     {id: 'overview', name: 'Overview', component: <Overview/>},
- // {id: 'solver', name: 'Solver', component: <div>GridEditor</div>},
- // {id: 'flow', name: 'Flow Package', component: <div>GridEditor</div>},
     {id: 'logs', name: 'Calculation logs', component: <Log/>},
     {id: 'files', name: 'Modflow files', component: <Files/>},
- // {id: 'calibration', name: 'Calibration data', component: <div>GridEditor</div>},
 ];
 
 const calculation = () => <SubMenuWithContent menuItems={menuItems}/>;
