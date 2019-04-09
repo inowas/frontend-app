ceeaa2f4544c630b4dfa057615f8e43281348fad
Ralf Junghanns <ralf.junghanns@gmail.com>
2019-04-09 13:36:50 +0200

    Upgrade disPackageProperties

--- src/scenes/t03/components/content/flow/mf/disPackageProperties.js
+++ src/scenes/t03/components/content/flow/mf/disPackageProperties.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {Form, Grid, Header, Input} from 'semantic-ui-react';
+import {Form, Grid, Header, Input, Table} from 'semantic-ui-react';
 
 import AbstractPackageProperties from './AbstractPackageProperties';
 import {FlopyModflowMfdis} from 'core/model/flopy/packages/mf';
@@ -116,89 +116,74 @@ class DisPackageProperties extends AbstractPackageProperties {
                             </Grid.Column>
                         ))}
                     </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Header as={'p'}>Stress Periods </Header>
+                            <Table basic>
+                                <Table.Header>
+                                    <Table.Row>
+                                        <Table.HeaderCell>#</Table.HeaderCell>
+                                        <Table.HeaderCell>Perlen</Table.HeaderCell>
+                                        <Table.HeaderCell>Nstp</Table.HeaderCell>
+                                        <Table.HeaderCell>Tsmult</Table.HeaderCell>
+                                        <Table.HeaderCell>Steady</Table.HeaderCell>
+                                    </Table.Row>
+                                </Table.Header>
+                                <Table.Body>
+                                    {mfPackage.perlen.map((perlen, idx) => (
+                                        <Table.Row key={idx}>
+                                            <Table.Cell>{idx + 1}</Table.Cell>
+                                            <Table.Cell>{perlen}</Table.Cell>
+                                            <Table.Cell>{mfPackage.nstp[idx]}</Table.Cell>
+                                            <Table.Cell>{mfPackage.tsmult[idx]}</Table.Cell>
+                                            <Table.Cell>{mfPackage.steady[idx] ? 'true' : 'false'}</Table.Cell>
+                                        </Table.Row>
+                                    ))}
+                                </Table.Body>
+                            </Table>
+                        </Grid.Column>
+                    </Grid.Row>
                     <Grid.Row/>
                 </Grid>
 
-                <Form.Group widths='equal'>
+                <Form.Group>
                     <Form.Field width={4}>
-                        <label>Time steps in stress period (nstp)</label>
-                        <Input readOnly
-                               name='nstp'
-                               value={JSON.stringify(mfPackage.nstp)}
-                               icon={this.renderInfoPopup(documentation.nstp, 'nstp')}
-                        />
-                    </Form.Field>
-                    <Form.Field width={4}>
-                        <label>Time step multiplier (tsmult)</label>
-                        <Input readOnly
-                               name='tsmult'
-                               value={JSON.stringify(mfPackage.tsmult)}
-                               icon={this.renderInfoPopup(documentation.tsmult, 'tsmult')}
-                        />
-                    </Form.Field>
-                    <Form.Field width={4}>
-                        <label>State of stress period (steady)</label>
-                        <Input readOnly
-                               name='steady'
-                               value={JSON.stringify(mfPackage.steady)}
-                               icon={this.renderInfoPopup(documentation.steady, 'steady')}
-                        />
-                    </Form.Field>
-                    <Form.Field width={4}>
-                        <label>Stress period lengths (perlen)</label>
-                        <Input readOnly
-                               name='perlen'
-                               value={JSON.stringify(mfPackage.perlen)}
-                               icon={this.renderInfoPopup(documentation.perlen, 'perlen')}
-                        />
-                    </Form.Field>
-                </Form.Group>
-
-                <Form.Group widths='equal'>
-                    <Form.Field>
                         <label>Time units (itmuni)</label>
-                        <Input readOnly
-                               type={'number'}
-                               name='itmuni'
-                               value={JSON.stringify(mfPackage.itmuni)}
-                               icon={this.renderInfoPopup(documentation.itmuni, 'itmuni')}
+                        <Input
+                            readOnly
+                            type={'number'}
+                            name='itmuni'
+                            value={mfPackage.itmuni}
+                            icon={this.renderInfoPopup(documentation.itmuni, 'itmuni')}
                         />
                     </Form.Field>
-                    <Form.Field>
+                    <Form.Field width={4}>
                         <label>Length units (lenuni)</label>
-                        <Input readOnly
-                               type={'number'}
-                               name='lenuni'
-                               value={JSON.stringify(mfPackage.lenuni)}
-                               icon={this.renderInfoPopup(documentation.lenuni, 'lenuni')}
+                        <Input
+                            readOnly
+                            type={'number'}
+                            name='lenuni'
+                            value={mfPackage.lenuni}
+                            icon={this.renderInfoPopup(documentation.lenuni, 'lenuni')}
                         />
                     </Form.Field>
-                </Form.Group>
-
-                <Form.Group widths='equal'>
-                    <Form.Field>
+                    <Form.Field width={4}>
                         <label>Filename extension</label>
-                        <Input readOnly
-                               name='extension'
-                               value={mfPackage.extension}
-                               icon={this.renderInfoPopup(documentation.extension, 'extension')}
+                        <Input
+                            readOnly
+                            name='extension'
+                            value={mfPackage.extension || ''}
+                            icon={this.renderInfoPopup(documentation.extension, 'extension')}
                         />
                     </Form.Field>
-                    <Form.Field>
+                    <Form.Field width={4}>
                         <label>File unit number</label>
-                        <Input readOnly
-                               type={'number'}
-                               name='unitnumber'
-                               value={JSON.stringify(mfPackage.unitnumber)}
-                               icon={this.renderInfoPopup(documentation.unitnumber, 'unitnumber')}
-                        />
-                    </Form.Field>
-                    <Form.Field>
-                        <label>Filenames</label>
-                        <Input readOnly
-                               name='filenames'
-                               value={JSON.stringify(mfPackage.filenames)}
-                               icon={this.renderInfoPopup(documentation.filenames, 'filenames')}
+                        <Input
+                            readOnly
+                            type={'number'}
+                            name='unitnumber'
+                            value={mfPackage.unitnumber || ''}
+                            icon={this.renderInfoPopup(documentation.unitnumber, 'unitnumber')}
                         />
                     </Form.Field>
                 </Form.Group>
@@ -206,29 +191,32 @@ class DisPackageProperties extends AbstractPackageProperties {
                 <Form.Group widths='equal'>
                     <Form.Field>
                         <label>Upper left corner x coordinate (xul)</label>
-                        <Input readOnly
-                               type={'number'}
-                               name='xul'
-                               value={JSON.stringify(mfPackage.xul)}
-                               icon={this.renderInfoPopup(documentation.xul, 'xul')}
+                        <Input
+                            readOnly
+                            type={'number'}
+                            name='xul'
+                            value={mfPackage.xul || ''}
+                            icon={this.renderInfoPopup(documentation.xul, 'xul')}
                         />
                     </Form.Field>
                     <Form.Field>
                         <label>Upper left corner y coordinate (yul)</label>
-                        <Input readOnly
-                               type={'number'}
-                               name='yul'
-                               value={JSON.stringify(mfPackage.yul)}
-                               icon={this.renderInfoPopup(documentation.yul, 'yul')}
+                        <Input
+                            readOnly
+                            type={'number'}
+                            name='yul'
+                            value={mfPackage.yul || ''}
+                            icon={this.renderInfoPopup(documentation.yul, 'yul')}
                         />
                     </Form.Field>
                     <Form.Field>
                         <label>Rotation</label>
-                        <Input readOnly
-                               type={'number'}
-                               name='rotation'
-                               value={JSON.stringify(mfPackage.rotation)}
-                               icon={this.renderInfoPopup(documentation.rotation, 'rotation')}
+                        <Input
+                            readOnly
+                            type={'number'}
+                            name='rotation'
+                            value={mfPackage.rotation || 0}
+                            icon={this.renderInfoPopup(documentation.rotation, 'rotation')}
                         />
                     </Form.Field>
                 </Form.Group>
@@ -236,19 +224,21 @@ class DisPackageProperties extends AbstractPackageProperties {
                 <Form.Group widths='equal'>
                     <Form.Field widths='equal'>
                         <label>Coordinate system (proj4_str)</label>
-                        <Input readOnly
-                               name='proj4_str'
-                               value={mfPackage.proj4_str}
-                               icon={this.renderInfoPopup(documentation.proj4_str, 'proj4_str')}
+                        <Input
+                            readOnly
+                            name='proj4_str'
+                            value={mfPackage.proj4_str || ''}
+                            icon={this.renderInfoPopup(documentation.proj4_str, 'proj4_str')}
                         />
                     </Form.Field>
                     <Form.Field>
                         <label>Starting date time (start_dateteim)</label>
-                        <Input readOnly
-                               type={'date'}
-                               name='start_dateteim'
-                               value={mfPackage.start_datetime}
-                               icon={this.renderInfoPopup(documentation.start_datetime, 'start_datetime')}
+                        <Input
+                            readOnly
+                            type={'date'}
+                            name='start_dateteim'
+                            value={mfPackage.start_datetime || ''}
+                            icon={this.renderInfoPopup(documentation.start_datetime, 'start_datetime')}
                         />
                     </Form.Field>
                 </Form.Group>
