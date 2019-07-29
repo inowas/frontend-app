9f954d872fc9af87927fe6dfba6e89ae9b1ac29c
Ralf Junghanns <ralf.junghanns@gmail.com>
2019-07-29 15:11:26 +0200

    Add Create-Scenario-Analysis Scene

---
+++ src/scenes/t03/components/maps/ModelMap.tsx
@@ -0,0 +1,115 @@
+import {LatLngExpression} from 'leaflet';
+import {uniqueId} from 'lodash';
+import React, {Component} from 'react';
+import {CircleMarker, GeoJSON, Map, Polygon, Polyline} from 'react-leaflet';
+import {WellBoundary} from '../../../../core/model/modflow/boundaries';
+import {Boundary, BoundaryCollection, Geometry} from '../../../../core/model/modflow/index';
+
+import {getStyle} from '../../../../services/geoTools/mapHelpers';
+import {BasicTileLayer} from '../../../../services/geoTools/tileLayers';
+
+export interface IPropsModelMap {
+    geometry: Geometry;
+    boundaries: BoundaryCollection | null;
+}
+
+const style = {
+    map: {
+        height: '400px',
+    }
+};
+
+class ModelMap extends Component<IPropsModelMap> {
+
+    // noinspection JSMethodCanBeStatic
+    public renderBoundaryGeometry(b: Boundary, underlay = false) {
+        const geometry = b.geometry;
+
+        if (!geometry) {
+            return;
+        }
+
+        if (underlay) {
+            switch (geometry.type.toLowerCase()) {
+                case 'point':
+                    return (
+                        <CircleMarker
+                            key={uniqueId(Geometry.fromObject(geometry).hash())}
+                            center={[
+                                geometry.coordinates[1],
+                                geometry.coordinates[0]
+                            ]}
+                            {...getStyle('underlay')}
+                        />
+                    );
+                case 'linestring':
+                    return (
+                        <Polyline
+                            key={uniqueId(Geometry.fromObject(geometry).hash())}
+                            positions={Geometry.fromObject(geometry).coordinatesLatLng}
+                            {...getStyle('underlay')}
+                        />
+                    );
+                default:
+                    return null;
+            }
+        }
+
+        switch (geometry.type.toLowerCase()) {
+            case 'point':
+                return b instanceof WellBoundary ? (
+                    <CircleMarker
+                        key={uniqueId(Geometry.fromObject(geometry).hash())}
+                        center={[
+                            geometry.coordinates[1],
+                            geometry.coordinates[0]
+                        ]}
+                        {...getStyle(b.type, b.wellType)}
+                    />
+                ) : null;
+            case 'linestring':
+                return (
+                    <Polyline
+                        key={uniqueId(Geometry.fromObject(geometry).hash())}
+                        positions={Geometry.fromObject(geometry).coordinatesLatLng as LatLngExpression[]}
+                    />
+                );
+            case 'polygon':
+                return (
+                    <Polygon
+                        key={uniqueId(Geometry.fromObject(geometry).hash())}
+                        positions={Geometry.fromObject(geometry).coordinatesLatLng as LatLngExpression[]}
+                    />
+                );
+            default:
+                return null;
+        }
+    }
+
+    public renderOtherBoundaries(boundaries: BoundaryCollection) {
+        return boundaries.boundaries
+            .map((b: Boundary) => this.renderBoundaryGeometry(b, true));
+    }
+
+    public render() {
+        const {geometry, boundaries} = this.props;
+
+        return (
+            <Map
+                style={style.map}
+                zoomControl={false}
+                bounds={geometry.getBoundsLatLng()}
+            >
+                <BasicTileLayer/>
+                <GeoJSON
+                    key={geometry.hash()}
+                    data={geometry.toGeoJSON()}
+                    style={getStyle('area')}
+                />
+                {boundaries && this.renderOtherBoundaries(boundaries)}
+            </Map>
+        );
+    }
+}
+
+export default ModelMap;
