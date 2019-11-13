62ca05064dcea0f8f66f83ce5b9f5a692f50ae60
Ralf Junghanns <ralf.junghanns@gmail.com>
2019-11-13 11:19:31 +0100

    Add env-variable to disable tools

--- src/scenes/dashboard/components/ToolsMenu.js
+++ src/scenes/dashboard/components/ToolsMenu.js
@@ -4,11 +4,20 @@ import {Icon, Menu} from 'semantic-ui-react';
 import {includes} from 'lodash';
 import {pure} from 'recompose';
 
+import getConfig from '../../../config.default';
+
+const {DISABLE_TOOL} = getConfig();
+
+const disabledTools = DISABLE_TOOL.split(',').map(s => s.trim()).map(s => s.toUpperCase());
+const isDisabled = (tool) => disabledTools.findIndex((e) => e === tool) >= 0;
+
+console.log(isDisabled('T10'));
+
 const ToolsMenu = ({activeTool, onClick, roles, tools}) => {
     return (
         <Menu fluid vertical>
             <Menu.Item header icon size='small'><Icon name="horizontal sliders"/>Tools</Menu.Item>
-            {tools.filter(t => includes(roles, t.role))
+            {tools.filter(t => includes(roles, t.role)).filter((t) => !isDisabled(t.slug))
                 .map((tool, key) => (
                     <Menu.Item
                         key={key}
