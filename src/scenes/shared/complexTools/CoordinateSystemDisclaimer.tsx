db9e1ad1e7970dd40447196fae4a9eca9f7c936b
Ralf Junghanns <ralf.junghanns@gmail.com>
2019-11-27 15:13:13 +0100

    Fix issue #1791

---
+++ src/scenes/shared/complexTools/CoordinateSystemDisclaimer.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Divider, Grid, Header} from 'semantic-ui-react';
+
+const coordinateSystemDisclaimer = () => (
+    <div>
+        <Grid columns={1} stackable={true} textAlign="center">
+            <Grid.Row>
+                <Grid.Column>
+                    <Header as={'h2'}>Geometry format</Header>
+                    {/* tslint:disable-next-line:max-line-length */}
+                    The platform is using the GEOJSON Format described in <a target={'_blank'} href={'https://tools.ietf.org/html/rfc7946'}>RFC7946.</a>.
+                    <br/>
+                    GEOJSON uses the geographic coordinate reference system: <br/>
+                    <br/>
+                    <strong>WGS84 with units of decimal degrees</strong>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+        <Divider/>
+    </div>
+);
+
+export default coordinateSystemDisclaimer;
