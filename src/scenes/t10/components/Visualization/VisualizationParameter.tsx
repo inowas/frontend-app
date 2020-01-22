6af167716da75269009da85e69d871659fd7f77a
Robert <robert.schlick@hydwis.de>
2020-01-22 14:16:48 +0100

    Feature/1775 - Sensor data monitoring
    * Improve ui
    * Add option to select time range

--- src/scenes/t10/components/Visualization/VisualizationParameter.tsx
+++ src/scenes/t10/components/Visualization/VisualizationParameter.tsx
@@ -1,10 +1,18 @@
+import {XYDataPoint} from 'downsample/dist/types';
 import {LatLngExpression} from 'leaflet';
 import {cloneDeep} from 'lodash';
 import moment from 'moment';
-import React, {useEffect, useRef, useState} from 'react';
+import React, {MouseEvent, useEffect, useRef, useState} from 'react';
 import {CircleMarker, Map, Tooltip} from 'react-leaflet';
 import {ReferenceLine, ResponsiveContainer, Scatter, ScatterChart, XAxis, YAxis} from 'recharts';
-import {Dimmer, Divider, Label, Loader, Segment} from 'semantic-ui-react';
+import {
+    Button,
+    ButtonProps,
+    Dimmer,
+    Grid, Input,
+    Loader, Popup,
+    Segment
+} from 'semantic-ui-react';
 import {BoundingBox} from '../../../../core/model/geometry';
 import {DataSourceCollection, Rtm} from '../../../../core/model/rtm';
 import {ProcessingCollection} from '../../../../core/model/rtm/processing';
@@ -13,13 +21,11 @@ import {BasicTileLayer} from '../../../../services/geoTools/tileLayers';
 import {ColorLegend} from '../../../shared/rasterData';
 import {rainbowFactory} from '../../../shared/rasterData/helpers';
 import {heatMapColors} from '../../../t05/defaults/gis';
-import {TimeSlider} from './index';
 import {IParameterWithMetaData} from './types';
 
 interface IProps {
     parameters: IParameterWithMetaData[];
     rtm: Rtm;
-    showScale: boolean;
 }
 
 interface ITimeStamps {
@@ -29,6 +35,15 @@ interface ITimeStamps {
     maxV: number;
     timestamps: number[];
 }
+
+const getClosestValue = (array: number[], value: number) => {
+    if (array.length > 0) {
+        return array.reduce((prev, curr) => {
+            return (Math.abs(curr - value) < Math.abs(prev - value) ? curr : prev);
+        });
+    }
+    return NaN;
+};
 
 const getData = (
     parameters: IParameterWithMetaData[],
@@ -67,6 +82,7 @@ const getData = (
                 });
                 key++;
                 getData(parameters, onFinished, data, tsData, key);
+                return;
             });
         });
         return;
@@ -75,13 +91,44 @@ const getData = (
     return onFinished(data, tsData);
 };
 
+const processData = (data: IParameterWithMetaData[], tsData: ITimeStamps) => {
+    return data.map((sensorData) => {
+        const d: XYDataPoint[] = [];
+        sensorData.data.forEach((row) => {
+            if (row.x >= tsData.minT && row.x <= tsData.maxT) {
+                d.push(row);
+            }
+        });
+        sensorData.data = d;
+        return sensorData;
+    });
+};
+
+enum tools {
+    DATETIME = 'datetime',
+    DATETIME_MIN = 'datetimeMin',
+    DATETIME_MAX = 'datetimeMax'
+}
+
 const visualizationParameter = (props: IProps) => {
     const [isAnimated, setIsAnimated] = useState<boolean>(false);
-    const [data, setData] = useState<IParameterWithMetaData[]>([]);
-    const [tsData, setTsData] = useState<ITimeStamps>({minT: 0, maxT: 0, minV: 0, maxV: 0, timestamps: []});
     const [isFetching, setIsFetching] = useState<boolean>(true);
+
+    const [activeTool, setActiveTool] = useState<tools>(tools.DATETIME);
+    const [withLines, setWithLines] = useState<boolean>(true);
+
     const [timestamp, setTimestamp] = useState<number>(0);
     const [intervalId, setIntervalId] = useState();
+
+    const [data, setData] = useState<IParameterWithMetaData[]>([]);
+    const [filteredData, setFilteredData] = useState<IParameterWithMetaData[]>([]);
+
+    const [showScale, setShowScale] = useState<boolean>(false);
+
+    const [tsData, setTsData] = useState<ITimeStamps>({minT: 0, maxT: 0, minV: 0, maxV: 0, timestamps: []});
+    const [filteredTsData, setFilteredTsData] = useState<ITimeStamps>(
+        {minT: 0, maxT: 0, minV: 0, maxV: 0, timestamps: []}
+    );
 
     const timeRef = useRef<number>(0);
 
@@ -91,30 +138,48 @@ const visualizationParameter = (props: IProps) => {
     }, heatMapColors.default);
 
     useEffect(() => {
-        getData(cloneDeep(props.parameters), (rData, rTsData) => {
-            setTsData(rTsData);
-            setData(rData);
-            setIsFetching(false);
-            setTimestamp(0);
-        });
+        if (props.parameters.length > 0) {
+            getData(cloneDeep(props.parameters), (rData, rTsData) => {
+                setTsData(rTsData);
+                setFilteredTsData(cloneDeep(rTsData));
+                setData(rData);
+                setFilteredData(cloneDeep(rData));
+                setIsFetching(false);
+                setTimestamp(rTsData.timestamps[0]);
+            });
+        }
     }, [props.parameters]);
 
     useEffect(() => {
         if (isAnimated) {
-            timeRef.current = timestamp;
+            timeRef.current = filteredTsData.timestamps.indexOf(timestamp);
             setIntervalId(setInterval(() => {
-                if (timeRef.current === tsData.timestamps.length - 1) {
+                if (timeRef.current === filteredTsData.timestamps.length - 1) {
                     timeRef.current = 0;
                 } else {
                     timeRef.current = timeRef.current + 1;
                 }
-                setTimestamp(timeRef.current);
+                setTimestamp(filteredTsData.timestamps[timeRef.current]);
             }, 1000));
         } else {
             clearTimeout(intervalId);
-            setTimestamp(timeRef.current);
+            setTimestamp(filteredTsData.timestamps[timeRef.current]);
         }
     }, [isAnimated]);
+
+    useEffect(() => {
+        const indexOfStart = tsData.timestamps.indexOf(getClosestValue(filteredTsData.timestamps, filteredTsData.minT));
+        const indexOfEnd = tsData.timestamps.indexOf(getClosestValue(filteredTsData.timestamps, filteredTsData.maxT));
+        const filtered = cloneDeep(tsData.timestamps).slice(indexOfStart, indexOfEnd);
+        setFilteredTsData({
+            ...filteredTsData,
+            timestamps: filtered
+        });
+        setFilteredData(processData(cloneDeep(data), {
+            ...filteredTsData,
+            timestamps: filtered
+        }));
+    }, [filteredTsData.minT, filteredTsData.maxT]);
 
     // tslint:disable-next-line:variable-name
     const RenderNoShape = () => null;
@@ -127,12 +192,46 @@ const visualizationParameter = (props: IProps) => {
         );
     }
 
-    const handleChangeSlider = (ts: number) => {
-        const index = tsData.timestamps.indexOf(ts);
-        return setTimestamp(index);
+    const handleClickReset = () => {
+        setTimestamp(tsData.timestamps[0]);
+        setFilteredTsData(cloneDeep(tsData));
     };
 
+    const handleClickLines = () => {
+        setWithLines(!withLines);
+    };
+
+    const handleClickChart = (e: any) => {
+        if (activeTool === tools.DATETIME && e && e.xValue && typeof e.xValue === 'number') {
+            return setTimestamp(getClosestValue(filteredTsData.timestamps, e.xValue));
+        }
+        if (activeTool === tools.DATETIME_MIN) {
+            const datetime = Math.floor(e.xValue);
+            if (timestamp < datetime) {
+                setTimestamp(getClosestValue(filteredTsData.timestamps, datetime));
+            }
+            return setFilteredTsData({
+                ...filteredTsData,
+                minT: datetime
+            });
+        }
+        if (activeTool === tools.DATETIME_MAX) {
+            const datetime = Math.floor(e.xValue);
+            if (timestamp > datetime) {
+                setTimestamp(getClosestValue(filteredTsData.timestamps, datetime));
+            }
+            return setFilteredTsData({
+                ...filteredTsData,
+                maxT: datetime
+            });
+        }
+    };
+
+    const handleSelectTool = (e: MouseEvent, {name}: ButtonProps) => setActiveTool(name);
+
     const handleTogglePlay = () => setIsAnimated(!isAnimated);
+
+    const handleToggleScale = () => setShowScale(!showScale);
 
     const calculateBoundingBox = () => {
         return BoundingBox.fromPoints(
@@ -169,10 +268,9 @@ const visualizationParameter = (props: IProps) => {
             let fillOpacity = 0.8;
             let value = null;
 
-            if (props.showScale && tsData) {
-                const row = parameter[0].data.filter((r) => r.x === tsData.timestamps[
-                    isAnimated ? timeRef.current : timestamp
-                    ]);
+            if (showScale && tsData) {
+                const row = parameter[0].data.filter((r) => r.x === (isAnimated ? tsData.timestamps[
+                    timeRef.current] : timestamp));
                 if (row.length > 0) {
                     value = row[0].y;
                     fillColor = `#${rainbow.colorAt(row[0].y)}`;
@@ -207,61 +305,164 @@ const visualizationParameter = (props: IProps) => {
 
     return (
         <div>
-            <TimeSlider
-                onChange={handleChangeSlider}
-                onTogglePlay={handleTogglePlay}
-                activeTimestamp={isAnimated ? timeRef.current : timestamp}
-                isAnimated={isAnimated}
-                timestamps={tsData.timestamps}
-            />
-            <Divider/>
-            <Map
-                zoom={1}
-                bounds={calculateBoundingBox().getBoundsLatLng()}
-                style={{
-                    height: '400px',
-                    width: '100%'
-                }}
-            >
-                <BasicTileLayer/>
-                {props.rtm.sensors.all.filter(
-                    (s) => props.parameters.filter((p) => p.meta.active && p.sensor.id === s.id).length > 0
-                ).map((s, sKey) => {
-                    return renderMarker(sKey, s.toObject());
-                })}
-                {props.showScale && renderLegend()}
-            </Map>
             <Segment color={'grey'} raised={true}>
-                <Label color={'blue'} ribbon={true} size={'large'}>
-                    Chart
-                </Label>
-                <ResponsiveContainer height={300}>
-                    <ScatterChart>
-                        <XAxis
-                            dataKey={'x'}
-                            domain={[tsData.minT, tsData.maxT]}
-                            name={'Date Time'}
-                            tickFormatter={(dt) => moment.unix(dt).format('YYYY/MM/DD')}
-                            type={'number'}
-                        />
-                        <YAxis dataKey={'y'} name={''} domain={['auto', 'auto']}/>
-                        {data.filter((r) => r.meta.active).map((row, idx) => (
-                            <Scatter
-                                key={idx}
-                                data={row.data}
-                                line={{strokeWidth: 2, stroke: row.meta.color}}
-                                lineType={'joint'}
-                                name={'p'}
-                                shape={<RenderNoShape/>}
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Button.Group>
+                                <Popup
+                                    content="Reset"
+                                    trigger={
+                                        <Button
+                                            onClick={handleClickReset}
+                                            icon="undo"
+                                        />
+                                    }
+                                />
+                                <Popup
+                                    content="Interpolation"
+                                    trigger={
+                                        <Button
+                                            onClick={handleClickLines}
+                                            icon="chart line"
+                                            primary={withLines}
+                                        />
+                                    }
+                                />
+                                <Popup
+                                    content="Play Animation"
+                                    trigger={
+                                        <Button
+                                            onClick={handleTogglePlay}
+                                            primary={isAnimated}
+                                            icon={isAnimated ? 'pause' : 'play'}
+                                        />
+                                    }
+                                />
+                            </Button.Group>
+                            &nbsp;
+                            <Button.Group>
+                                <Popup
+                                    content="Select time"
+                                    trigger={
+                                        <Button
+                                            onClick={handleSelectTool}
+                                            name={tools.DATETIME}
+                                            primary={activeTool === tools.DATETIME}
+                                            icon="mouse pointer"
+                                        />
+                                    }
+                                />
+                                <Popup
+                                    content="Select lower time limit"
+                                    trigger={
+                                        <Button
+                                            onClick={handleSelectTool}
+                                            name={tools.DATETIME_MIN}
+                                            primary={activeTool === tools.DATETIME_MIN}
+                                            icon="arrow alternate circle right"
+                                        />
+                                    }
+                                />
+                                <Popup
+                                    content="Select upper time limit"
+                                    trigger={
+                                        <Button
+                                            onClick={handleSelectTool}
+                                            name={tools.DATETIME_MAX}
+                                            primary={activeTool === tools.DATETIME_MAX}
+                                            icon="arrow alternate circle left"
+                                        />
+                                    }
+                                />
+                            </Button.Group>
+                            &nbsp;
+                            <Popup
+                                content="Selected time"
+                                trigger={
+                                    <Input
+                                        value={moment.unix(timestamp).format('YYYY-MM-DD HH:mm:ss')}
+                                        readOnly={true}
+                                    />
+                                }
                             />
-                        ))}
-                        <ReferenceLine
-                            x={tsData.timestamps[isAnimated ? timeRef.current : timestamp]}
-                            stroke="#000"
-                            strokeDasharray="3 3"
-                        />
-                    </ScatterChart>
-                </ResponsiveContainer>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <ResponsiveContainer height={300}>
+                                <ScatterChart onClick={handleClickChart}>
+                                    <XAxis
+                                        dataKey={'x'}
+                                        domain={[filteredTsData.minT, filteredTsData.maxT]}
+                                        name={'Date Time'}
+                                        tickFormatter={(dt) => moment.unix(dt).format('YYYY/MM/DD')}
+                                        type={'number'}
+                                    />
+                                    <YAxis dataKey={'y'} name={''} domain={[filteredTsData.minV, filteredTsData.maxV]}/>
+                                    {filteredData.filter((r) => r.meta.active).map((row, idx) => (
+                                        <Scatter
+                                            key={idx}
+                                            data={row.data}
+                                            fill={row.meta.color}
+                                            line={withLines ? {strokeWidth: 2, stroke: row.meta.color} : false}
+                                            lineType={'joint'}
+                                            name={'p'}
+                                            shape={withLines ? <RenderNoShape/> : 'cross'}
+                                        />
+                                    ))}
+                                    <ReferenceLine
+                                        x={isAnimated ? filteredTsData.timestamps[timeRef.current] : timestamp}
+                                        stroke="#000"
+                                        strokeDasharray="3 3"
+                                    />
+                                </ScatterChart>
+                            </ResponsiveContainer>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Segment>
+            <Segment color={'grey'} raised={true}>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Button.Group>
+                                <Popup
+                                    content="Show color scale"
+                                    trigger={
+                                        <Button
+                                            onClick={handleToggleScale}
+                                            icon="chart pie"
+                                            primary={showScale}
+                                        />
+                                    }
+                                />
+                            </Button.Group>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Map
+                                maxZoom={19}
+                                bounds={calculateBoundingBox().getBoundsLatLng()}
+                                style={{
+                                    height: '400px',
+                                    width: '100%'
+                                }}
+                            >
+                                <BasicTileLayer/>
+                                {props.rtm.sensors.all.filter(
+                                    (s) => props.parameters.filter(
+                                        (p) => p.meta.active && p.sensor.id === s.id
+                                    ).length > 0
+                                ).map((s, sKey) => {
+                                    return renderMarker(sKey, s.toObject());
+                                })}
+                                {showScale && renderLegend()}
+                            </Map>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </Segment>
         </div>
     );
