758bf3990d7c0090870de41f1cd2a58c6bc4afbb
Ralf Junghanns <ralf.junghanns@gmail.com>
2020-02-18 11:15:16 +0100

    Refactor indexes

--- src/scenes/t03/index.ts
+++ src/scenes/t03/index.ts
@@ -1,5 +1,5 @@
-import * as Container from './containers';
 import * as Component from './components/content/index';
+import * as Container from './containers';
 import * as Reducer from './reducers';
 
 export {
