9c1b8dd581bb9df949ef5fca64fa848a2161c1a7
Robert Schlick <robert.schlick@hydwis.de>
2020-04-16 15:29:06 +0200

    Fix seawat packages
    * Add messaging

--- src/scenes/t03/components/content/seawat/packages/VdfPackageProperties.tsx
+++ src/scenes/t03/components/content/seawat/packages/VdfPackageProperties.tsx
@@ -23,7 +23,7 @@ const vdfPackageProperties = (props: IProps) => {
 
     const handleOnSelect = (e: SyntheticEvent, {name, value}: DropdownProps) => {
         setSwtPackage({...swtPackage, [name]: value});
-        props.onChange(FlopySeawatSwtvdf.fromObject({...swtPackage, [name]: value}));
+        props.onChange(FlopySeawatSwtvdf.fromObject({...props.swtPackage, [name]: value}));
     };
 
     const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -79,7 +79,7 @@ const vdfPackageProperties = (props: IProps) => {
                         readOnly={readOnly}
                         name="denseref"
                         value={swtPackage.denseref}
-                        onBlur={handleOnBlur}
+                        onBlur={handleOnBlur(parseFloat)}
                         onChange={handleOnChange}
                         icon={renderInfoPopup(documentation.denseref, 'denseref', PopupPosition.BOTTOM_RIGHT)}
                         type="number"
@@ -93,7 +93,7 @@ const vdfPackageProperties = (props: IProps) => {
                         readOnly={readOnly}
                         name="drhodc"
                         value={swtPackage.drhodc}
-                        onBlur={handleOnBlur}
+                        onBlur={handleOnBlur(parseFloat)}
                         onChange={handleOnChange}
                         icon={renderInfoPopup(documentation.drhodc, 'drhodc', PopupPosition.BOTTOM_RIGHT)}
                         type="number"
@@ -160,8 +160,8 @@ const vdfPackageProperties = (props: IProps) => {
                     <Input
                         readOnly={readOnly}
                         name="nswtcpl"
-                        value={swtPackage.nswtcpl || ''}
-                        onBlur={handleOnBlur}
+                        value={swtPackage.nswtcpl}
+                        onBlur={handleOnBlur(parseFloat)}
                         onChange={handleOnChange}
                         icon={renderInfoPopup(documentation.nswtcpl, 'nswtcpl')}
                         type="number"
@@ -172,8 +172,8 @@ const vdfPackageProperties = (props: IProps) => {
                     <Input
                         readOnly={readOnly}
                         name="dnscrit"
-                        value={swtPackage.dnscrit || ''}
-                        onBlur={handleOnBlur}
+                        value={swtPackage.dnscrit}
+                        onBlur={handleOnBlur(parseFloat)}
                         onChange={handleOnChange}
                         icon={renderInfoPopup(documentation.dnscrit, 'dnscrit', PopupPosition.TOP_RIGHT)}
                         type="number"
@@ -188,8 +188,8 @@ const vdfPackageProperties = (props: IProps) => {
                     <Input
                         readOnly={readOnly}
                         name="dnscrit"
-                        value={swtPackage.dnscrit || ''}
-                        onBlur={handleOnBlur}
+                        value={swtPackage.dnscrit}
+                        onBlur={handleOnBlur(parseFloat)}
                         onChange={handleOnChange}
                         icon={renderInfoPopup(documentation.dnscrit, 'dnscrit', PopupPosition.TOP_RIGHT)}
                         type="number"
@@ -203,8 +203,8 @@ const vdfPackageProperties = (props: IProps) => {
                     <Input
                         readOnly={readOnly}
                         name="densemin"
-                        value={swtPackage.densemin || 0}
-                        onBlur={handleOnBlur}
+                        value={swtPackage.densemin}
+                        onBlur={handleOnBlur(parseFloat)}
                         onChange={handleOnChange}
                         icon={renderInfoPopup(documentation.densemin, 'densemin')}
                         type="number"
@@ -215,8 +215,8 @@ const vdfPackageProperties = (props: IProps) => {
                     <Input
                         readOnly={readOnly}
                         name="densemax"
-                        value={swtPackage.densemax || 0}
-                        onBlur={handleOnBlur}
+                        value={swtPackage.densemax}
+                        onBlur={handleOnBlur(parseFloat)}
                         onChange={handleOnChange}
                         icon={renderInfoPopup(documentation.densemax, 'densemax', PopupPosition.TOP_RIGHT)}
                         type="number"
