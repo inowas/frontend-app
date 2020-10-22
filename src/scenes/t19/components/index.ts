702bc8349a68c4d98cf10619dae4d055c20ab08e
Ralf Junghanns <ralf.junghanns@gmail.com>
2020-10-22 12:06:53 +0200

    Improvements T19
    #1951

--- src/scenes/t19/components/index.ts
+++ src/scenes/t19/components/index.ts
@@ -1,9 +1,11 @@
 import HeatTransportController from './HeatTransportController';
 import HeatTransportInput from './HeatTransportInput';
+import HeatTransportInputChart from './HeatTransportInputChart';
 import HeatTransportResults from './HeatTransportResults';
 
 export {
     HeatTransportController,
     HeatTransportInput,
+    HeatTransportInputChart,
     HeatTransportResults
 };
