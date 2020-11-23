4d6d1f9b3b400ccce8d4d21d8dbac79053e7f246
Robert Schlick <robert.schlick@hydwis.de>
2020-11-23 13:32:51 +0100

    Feature/1999 - T19: Fixes and improvements
    * Switch to line charts for better tooltips

---
+++ src/scenes/t19/components/CustomTooltip.tsx
@@ -0,0 +1,59 @@
+import {TooltipProps} from 'recharts';
+import React from 'react';
+import moment from 'moment';
+
+const styles = {
+    customTooltip: {
+        margin: 0,
+        lineHeight: '24px',
+        border: '1px solid #f5f5f5',
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        padding: '10px'
+    },
+
+    item: {
+        borderTop: '1px solid #f5f5f5',
+        margin: 0,
+        textAlign: 'right' as CanvasTextAlign
+    },
+
+    label: {
+        margin: 0,
+        textAlign: 'center' as CanvasTextAlign
+    }
+};
+
+type IProps = TooltipProps & {
+    colors: [string, string];
+    dateTimeFormat: string;
+};
+
+const CustomTooltip = (props: IProps) => {
+    if (!props.payload || props.payload.length < 1) {
+        return null;
+    }
+
+    const renderDate = () => {
+        if (!props.label) {
+            return null;
+        }
+        if (typeof props.label === 'number') {
+            return moment.unix(props.label).format(props.dateTimeFormat);
+        }
+        return props.label;
+    };
+
+    return (
+        <div style={styles.customTooltip}>
+            <p style={styles.label}>{renderDate()}</p>
+            {props.payload.map((row, key) => (
+                <p key={key} style={{
+                    ...styles.item,
+                    color: props.colors.length > key ? props.colors[key] : '#000000'
+                }}>{row.dataKey}: {typeof row.value === 'number' ? row.value.toFixed(2) : row.value}°C</p>
+            ))}
+        </div>
+    );
+};
+
+export default CustomTooltip;
---
+++ src/scenes/t19/components/HeatTransportResultLineChart.tsx
@@ -0,0 +1,138 @@
+import {
+    CartesianGrid,
+    Label, Line, LineChart,
+    ReferenceDot,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis
+} from 'recharts';
+import {IHeatTransportResults} from '../../../core/model/htm/Htm.type';
+import {SemanticCOLORS} from 'semantic-ui-react/dist/commonjs/generic';
+import CustomTooltip from './CustomTooltip';
+import React, {useEffect, useState} from 'react';
+import _ from 'lodash';
+import moment from 'moment';
+
+interface IReferencePoint {
+    fill: SemanticCOLORS;
+    x: number;
+    y: number;
+    type: string;
+}
+
+interface IProps {
+    data: Array<{
+        x: number;
+        obs: number;
+        sim: number
+    }>;
+    dateTimeFormat: string;
+    results: IHeatTransportResults;
+    timesteps?: [number, number];
+    type: string;
+    useSameTimes?: boolean;
+}
+
+const HeatTransportResultChart = (props: IProps) => {
+    const updateDomain = (p: IReferencePoint[]): [number, number] => {
+        if (props.timesteps && props.useSameTimes) {
+            return props.timesteps;
+        }
+        const pointsOrderedByX = _.sortBy(p, 'x');
+        const xMin = props.data[0].x < pointsOrderedByX[0].x ? props.data[0].x : pointsOrderedByX[0].x;
+        const xMax = props.data[props.data.length - 1].x > pointsOrderedByX[pointsOrderedByX.length - 1].x ?
+            props.data[props.data.length - 1].x : pointsOrderedByX[pointsOrderedByX.length - 1].x;
+        return [xMin - 10, xMax + 100000];
+    };
+
+    const [domain, setDomain] = useState<[number, number]>();
+    const [points, setPoints] = useState<IReferencePoint[]>()
+
+    useEffect(() => {
+        const p = props.results.points.filter((point) => point.label.includes(props.type)).map((point) => ({
+            fill: point.point_type === 'max' ? 'red' : (point.point_type === 'min' ? 'green' : 'blue'),
+            x: moment(point.date).unix(),
+            y: point.simulated,
+            type: point.point_type
+        } as IReferencePoint));
+        setPoints(p);
+        setDomain(updateDomain(p));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [props.data, props.results, props.timesteps]);
+
+    const formatDateTimeTicks = (dt: number) => {
+        return moment.unix(dt).format(props.dateTimeFormat);
+    };
+
+    const formatTemperatureTicks = (t: number) => {
+        return t.toFixed(2);
+    };
+
+    const getTicks = () => {
+        if (domain) {
+            const dateStart = moment.unix(domain[0]);
+            const dateEnd = moment.unix(domain[1]);
+            const interim = dateStart.clone();
+            const timeValues: number[] = [];
+
+            while (dateEnd > interim || interim.format('M') === dateEnd.format('M')) {
+                timeValues.push(moment(interim.format('YYYY-MM')).unix());
+                interim.add(1, 'month');
+            }
+            return timeValues;
+        }
+        return undefined;
+    };
+
+    if (!points || !domain) {
+        return null;
+    }
+
+    return (
+        <ResponsiveContainer height={300}>
+            <LineChart data={props.data}>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <XAxis
+                    dataKey={'x'}
+                    domain={domain}
+                    tickFormatter={formatDateTimeTicks}
+                    ticks={getTicks()}
+                    type={'number'}
+                />
+                <YAxis
+                    label={{value: 'T [°C]', angle: -90, position: 'insideLeft'}}
+                    tickFormatter={formatTemperatureTicks}
+                />
+                <Line dot={false} type="monotone" dataKey="obs" stroke="#db3434" strokeWidth={2}/>
+                <Line dot={false} type="monotone" dataKey="sim" stroke="#3498DB" strokeWidth={2}/>
+                <Tooltip content={
+                    <CustomTooltip colors={['#db3434', '#3498DB']} dateTimeFormat={props.dateTimeFormat}/>
+                }
+                />
+                {points.map((point, key) => (
+                    <ReferenceDot
+                        key={key}
+                        label={
+                            <Label
+                                fill={point.fill}
+                                fontSize={12}
+                                offset={5}
+                                position="top"
+                                value={point.type}
+                            />
+                        }
+                        x={point.x}
+                        y={point.y}
+                        r={10}
+                        fill={point.fill}
+                        fillOpacity={0.4}
+                        stroke="none"
+                    />
+                ))}
+            </LineChart>
+        </ResponsiveContainer>
+    );
+};
+
+export default HeatTransportResultChart;
