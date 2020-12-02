20ac2810b81da2dbd675bb335f5655565a2246e8
Ralf Junghanns <ralf.junghanns@gmail.com>
2020-12-02 13:26:46 +0100

    Fix/1934 show contour layers in background (#347)
    * Fix bug: Contour layers should be shown alwas in background

--- src/scenes/shared/rasterData/index.ts
+++ src/scenes/shared/rasterData/index.ts
@@ -1,13 +1,15 @@
 import ColorLegend from './ColorLegend';
 import ColorLegendDiscrete from './ColorLegendDiscrete';
+import CustomMap from './CustomMap';
 import RasterDataImage from './rasterDataImage';
-import rasterDataMap from './rasterDataMap';
 import RasterfileUploadModal from './rasterfileUploadModal';
 import ReactLeafletHeatMapCanvasOverlay from './ReactLeafletHeatMapCanvasOverlay';
+import rasterDataMap from './rasterDataMap';
 
 export {
     ColorLegend,
     ColorLegendDiscrete,
+    CustomMap,
     RasterDataImage,
     rasterDataMap as RasterDataMap,
     RasterfileUploadModal,
--- src/scenes/shared/rasterData/rasterDataMap.tsx
+++ src/scenes/shared/rasterData/rasterDataMap.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import {Children, LayersControl, Map} from 'react-leaflet';
 import {Array2D} from '../../../core/model/geometry/Array2D.type';
-import {ModflowModel} from '../../../core/model/modflow';
-import BoundaryCollection from '../../../core/model/modflow/boundaries/BoundaryCollection';
 import {BasicTileLayer} from '../../../services/geoTools/tileLayers';
-import {rainbowFactory} from '../../../services/rainbowvis/helpers';
-import Rainbow from '../../../services/rainbowvis/Rainbowvis';
+import {Children, LayersControl} from 'react-leaflet';
 import {ILegendItem} from '../../../services/rainbowvis/types';
-import {renderBoundaryOverlays, renderBoundingBoxLayer} from '../../t03/components/maps/mapLayers';
+import {ModflowModel} from '../../../core/model/modflow';
+import {max, min} from './helpers';
+import {rainbowFactory} from '../../../services/rainbowvis/helpers';
+import {renderBoundaryOverlays, renderBoundingBoxLayer, renderContourLayer} from '../../t03/components/maps/mapLayers';
+import BoundaryCollection from '../../../core/model/modflow/boundaries/BoundaryCollection';
 import ColorLegend from './ColorLegend';
-import ContourLayer from './contourLayer';
-import {
-    max,
-    min
-} from './helpers';
+import CustomMap from './CustomMap';
+import Rainbow from '../../../services/rainbowvis/Rainbowvis';
+import React from 'react';
 
 const styles = {
     map: {
@@ -22,7 +19,7 @@ const styles = {
     }
 };
 
-const renderLegend = (rainbow: Rainbow, unit: string = '') => {
+const renderLegend = (rainbow: Rainbow, unit = '') => {
     const gradients = rainbow.gradients.slice().reverse();
     const lastGradient = gradients[gradients.length - 1];
     const legend: ILegendItem[] = gradients.map((gradient) => ({
@@ -51,7 +48,7 @@ const RasterDataMap = (props: IProps) => {
     const rainbowVis = rainbowFactory({min: min(data), max: max(data)});
 
     return (
-        <Map
+        <CustomMap
             style={styles.map}
             zoomControl={false}
             bounds={model.boundingBox.getBoundsLatLng()}
@@ -63,18 +60,12 @@ const RasterDataMap = (props: IProps) => {
                 {renderBoundaryOverlays(props.boundaries)}
             </LayersControl>
             }
-            <ContourLayer
-                boundingBox={model.boundingBox}
-                data={data}
-                geometry={model.geometry}
-                gridSize={model.gridSize}
-                rainbow={rainbowVis}
-                rotation={model.rotation}
-                steps={0}
-            />
+
+            {renderContourLayer({model, data, rainbow: rainbowVis, steps: 0})}
+
             {children}
             {renderLegend(rainbowVis, unit)}
-        </Map>
+        </CustomMap>
     );
 };
 
