416f30d0eaaebff258be5b865932306efa5b9b68
Ralf Junghanns <ralf.junghanns@gmail.com>
2020-12-02 13:25:23 +0100

    Fix bug: Contour layers should be shown alwas in background

---
+++ src/scenes/shared/rasterData/CustomMap.js
@@ -0,0 +1,22 @@
+import {Map} from 'react-leaflet';
+
+export default class CustomBaseMap extends Map {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidUpdate(prevProps) {
+        this.updateLeafletElement(prevProps, this.props);
+        const layers = this.leafletElement._layers;
+        Object.values(layers)
+            .filter((layer) => {
+                return typeof layer.options.priority !== 'undefined';
+            })
+            .sort((layerA, layerB) => {
+                return layerA.options.priority - layerB.options.priority;
+            })
+            .forEach((layer) => {
+                layer.bringToFront();
+            });
+    }
+}
