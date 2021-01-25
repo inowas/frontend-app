f5270de8e360d10d26460a6f59f44044b9f70b0c
Robert Schlick <robert.schlick@hydwis.de>
2021-01-25 13:39:09 +0100

    Fix and refactor boundary import

--- src/scenes/t03/components/content/boundaries/boundaryComparator.tsx
+++ src/scenes/t03/components/content/boundaries/boundaryComparator.tsx
@@ -1,192 +1,207 @@
 import {BoundaryCollection} from '../../../../../core/model/modflow/boundaries';
 import {BoundarySelection, BoundaryType} from '../../../../../core/model/modflow/boundaries/Boundary.type';
-import {Button, Divider, Dropdown, DropdownProps, Grid, Header, Icon, Menu} from 'semantic-ui-react';
+import {
+  Button,
+  Checkbox,
+  Divider,
+  Dropdown,
+  DropdownProps,
+  Grid,
+  Header,
+  Icon,
+  Menu,
+  Segment
+} from 'semantic-ui-react';
 import {IBoundaryComparisonItem} from '../../../../../core/model/modflow/boundaries/BoundaryCollection';
 import {ModflowModel, Soilmodel} from '../../../../../core/model/modflow';
 import BoundaryDetailsImport from './boundaryDetailsImport';
 import BoundarySynchronizer from './boundarySychronizer';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 interface IBoundaryTypeObject {
-    key: BoundarySelection;
-    text: string;
-    value: BoundarySelection;
+  key: BoundarySelection;
+  text: string;
+  value: BoundarySelection;
 }
 
 interface IProps {
-    currentBoundaries: BoundaryCollection;
-    newBoundaries: BoundaryCollection;
-    model: ModflowModel;
-    soilmodel: Soilmodel;
-    selectedBoundary: string | null;
-    types?: BoundaryType[];
-    onBoundaryClick: (bid: string) => void;
+  currentBoundaries: BoundaryCollection;
+  newBoundaries: BoundaryCollection;
+  model: ModflowModel;
+  soilmodel: Soilmodel;
+  selectedBoundary: string | null;
+  types?: BoundaryType[];
+  onBoundaryClick: (bid: string) => void;
 }
 
-interface IState {
-    boundaryList: IBoundaryComparisonItem[];
-    selectedType: string;
-}
+const BoundaryComparator = (props: IProps) => {
+  const [boundaryList, setBoundaryList] = useState<IBoundaryComparisonItem[]>([]);
+  const [selectedItem, setSelectedItem] = useState<IBoundaryComparisonItem | null>(null);
+  const [selectedType, setSelectedType] = useState<string>('all');
+  const [removeExistingBoundaries, setRemoveExistingBoundaries] = useState<boolean>(false);
 
-class BoundaryComparator extends React.Component<IProps, IState> {
-    public constructor(props: IProps) {
-        super(props);
-        this.state = {
-            boundaryList: props.currentBoundaries.compareWith(props.model.stressperiods, props.newBoundaries),
-            selectedType: props.types && props.types.length === 1 ? props.types[0] : 'all'
-        };
+  useEffect(() => {
+    setSelectedType(props.types && props.types.length === 1 ? props.types[0] : 'all')
+  }, [props.types]);
+
+  useEffect(() => {
+    setBoundaryList(props.currentBoundaries.compareWith(
+      props.model.stressperiods, props.newBoundaries, removeExistingBoundaries
+    ));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.currentBoundaries]);
+
+  const noHandle = () => ({});
+
+  const boundaryTypes = (): IBoundaryTypeObject[] => {
+    if (props.types) {
+      const types = props.types.length > 1 ?
+        [{key: 'all' as BoundarySelection, value: 'all' as BoundarySelection, text: 'All'}] : [];
+      return types.concat(props.types.map((type: BoundarySelection) => {
+        return {key: type as BoundarySelection, value: type as BoundarySelection, text: type.toUpperCase()};
+      }));
     }
 
-    public UNSAFE_componentWillReceiveProps(nextProps: IProps) {
-        this.setState({
-            selectedType: nextProps.types && nextProps.types.length === 1 ? nextProps.types[0] : 'all',
-            boundaryList: nextProps.currentBoundaries.compareWith(
-                nextProps.model.stressperiods, nextProps.newBoundaries
-            )
-        });
+    return [
+      {key: 'all', value: 'all', text: 'All types'},
+      {key: 'chd', value: 'chd', text: 'CHD'},
+      {key: 'drn', value: 'drn', text: 'DRN'},
+      {key: 'evt', value: 'evt', text: 'EVT'},
+      {key: 'ghb', value: 'ghb', text: 'GHB'},
+      {key: 'hob', value: 'hob', text: 'HOB'},
+      {key: 'rch', value: 'rch', text: 'RCH'},
+      {key: 'riv', value: 'riv', text: 'RIV'},
+      {key: 'wel', value: 'wel', text: 'WEL'},
+    ];
+  };
+
+  const handleChangeRemoveBoundaries = () => setRemoveExistingBoundaries(!removeExistingBoundaries);
+
+  const handleClick = (id: string) => () => {
+    const item = boundaryList.filter((i) => i.id === id);
+    if (item.length > 0) {
+      setSelectedItem(item[0]);
+    }
+    props.onBoundaryClick(id);
+  }
+
+  const handleLocalChange = (e: React.SyntheticEvent<HTMLElement>, data: DropdownProps) =>
+    setSelectedType(data.value as BoundarySelection);
+
+  const renderList = () => {
+    let selectedBoundaryItems = boundaryList;
+    if (selectedType !== 'all') {
+      selectedBoundaryItems = selectedBoundaryItems.filter((b) => b.type === selectedType);
     }
 
-    public render() {
-        const {types, selectedBoundary} = this.props;
-        const {boundaryList} = this.state;
+    return (
+      <Menu
+        fluid={true}
+        vertical={true}
+        tabular={true}
+      >
+        {selectedBoundaryItems.map((b: IBoundaryComparisonItem) => (
+          <Menu.Item
+            name={b.name}
+            key={b.id}
+            active={b.id === props.selectedBoundary}
+            onClick={handleClick(b.id)}
+          >
+            {b.name}
+          </Menu.Item>
+        ))}
+        <Menu.Item>
+          <BoundarySynchronizer
+            currentBoundaries={props.currentBoundaries}
+            newBoundaries={props.newBoundaries}
+            model={props.model}
+            removeExistingBoundaries={removeExistingBoundaries}
+          />
+        </Menu.Item>
+      </Menu>
+    );
+  };
 
-        if (selectedBoundary === null) {
-            return null;
-        }
+  const {types, selectedBoundary} = props;
 
-        let boundary = this.props.newBoundaries.findById(selectedBoundary);
-        if (!boundary) {
-            boundary = this.props.currentBoundaries.findById(selectedBoundary);
-        }
+  if (selectedBoundary === null) {
+    return null;
+  }
 
-        return (
-            <div>
-                <Divider horizontal={true}>
-                    <Header as="h4">
-                        <Icon name="eye" />
-                        Preview Changes
-                    </Header>
-                </Divider>
+  let boundary = props.newBoundaries.findById(selectedBoundary);
+  if (!boundary) {
+    boundary = props.currentBoundaries.findById(selectedBoundary);
+  }
 
-                    <Grid stackable={true}>
-                        <Grid.Row>
-                            <Grid.Column width={4}>
-                                        {types && types.length === 1 ?
-                                            <Button
-                                                className="blue"
-                                                fluid={true}
-                                                icon={true}
-                                                labelPosition="left"
-                                            >
-                                                <Icon name="plus"/>
-                                                Add
-                                            </Button>
-                                            :
-                                                <Button as="div" labelPosition="left">
-                                                    <Dropdown
-                                                        selection={true}
-                                                        options={this.boundaryTypes().map((b) => {
-                                                            let numberOfBoundaries = boundaryList.length;
-                                                            if (b.value !== 'all') {
-                                                                numberOfBoundaries = boundaryList
-                                                                    .filter((e) => (e.type === b.value)).length;
-                                                            }
-
-                                                            const name = `${b.text} (${numberOfBoundaries})`;
-
-                                                            return {
-                                                                ...b,
-                                                                disabled: b.value !== 'all' && numberOfBoundaries === 0,
-                                                                text: name
-                                                            };
-                                                        })}
-                                                        onChange={this.handleLocalChange}
-                                                        value={this.state.selectedType}
-                                                    />
-                                                </Button>
-                                        }
-
-                                        {this.list()}
-
-                            </Grid.Column>
-                            <Grid.Column width={12}>
-                                {boundary && <BoundaryDetailsImport
-                                    boundary={boundary}
-                                    boundaries={this.props.newBoundaries}
-                                    model={this.props.model}
-                                    soilmodel={this.props.soilmodel}
-                                    onChange={this.noHandle}
-                                    onClick={this.noHandle}
-                                    readOnly={true}
-                                />}
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                <BoundarySynchronizer
-                    currentBoundaries={this.props.currentBoundaries}
-                    newBoundaries={this.props.newBoundaries}
-                    model={this.props.model}
-                />
-            </div>
-        );
-    }
-
-    private noHandle = () => ({});
-
-    private boundaryTypes = (): IBoundaryTypeObject[] => {
-        if (this.props.types) {
-            const types = this.props.types.length > 1 ?
-                [{key: 'all' as BoundarySelection, value: 'all' as BoundarySelection, text: 'All'}] : [];
-            return types.concat(this.props.types.map((type: BoundarySelection) => {
-                return {key: type as BoundarySelection, value: type as BoundarySelection, text: type.toUpperCase()};
-            }));
-        }
-
-        return [
-            {key: 'all', value: 'all', text: 'All types'},
-            {key: 'chd', value: 'chd', text: 'CHD'},
-            {key: 'drn', value: 'drn', text: 'DRN'},
-            {key: 'evt', value: 'evt', text: 'EVT'},
-            {key: 'ghb', value: 'ghb', text: 'GHB'},
-            {key: 'rch', value: 'rch', text: 'RCH'},
-            {key: 'riv', value: 'riv', text: 'RIV'},
-            {key: 'wel', value: 'wel', text: 'WEL'},
-        ];
-    };
-
-    private list = () => {
-        const {selectedType, boundaryList} = this.state;
-        let selectedBoundaryItems = boundaryList;
-        if (selectedType !== 'all') {
-            selectedBoundaryItems = selectedBoundaryItems.filter((b) => b.type === selectedType);
-        }
-
-        return (
-            <Menu
+  return (
+    <div>
+      <Divider horizontal={true}>
+        <Header as="h4">
+          <Icon name="eye"/>
+          Preview Changes
+        </Header>
+      </Divider>
+      <Segment>
+        <Checkbox
+          checked={removeExistingBoundaries}
+          label='Remove existing boundaries, which are not updated by the uploaded file'
+          onChange={handleChangeRemoveBoundaries}
+        />
+      </Segment>
+      <Grid stackable={true}>
+        <Grid.Row>
+          <Grid.Column width={4}>
+            {types && types.length === 1 ?
+              <Button
+                className="blue"
                 fluid={true}
-                vertical={true}
-                tabular={true}
-            >
-                {selectedBoundaryItems.map((b: IBoundaryComparisonItem) => (
-                    <Menu.Item
-                        name={b.name}
-                        key={b.id}
-                        active={b.id === this.props.selectedBoundary}
-                        onClick={this.handleClick(b.id)}
-                    >
-                        {b.name}
-                    </Menu.Item>
-                ))}
-            </Menu>
-        );
-    };
+                icon={true}
+                labelPosition="left"
+              >
+                <Icon name="plus"/>
+                Add
+              </Button>
+              :
+              <Button as="div" labelPosition="left">
+                <Dropdown
+                  selection={true}
+                  options={boundaryTypes().map((b) => {
+                    let numberOfBoundaries = boundaryList.length;
+                    if (b.value !== 'all') {
+                      numberOfBoundaries = boundaryList
+                        .filter((e) => (e.type === b.value)).length;
+                    }
 
-    private handleClick = (id: string) => () => this.props.onBoundaryClick(id);
+                    const name = `${b.text} (${numberOfBoundaries})`;
 
-    private handleLocalChange = (e: React.SyntheticEvent<HTMLElement>, data: DropdownProps) => this.setState({
-        selectedType: data.value as BoundarySelection
-    });
-
+                    return {
+                      ...b,
+                      disabled: b.value !== 'all' && numberOfBoundaries === 0,
+                      text: name
+                    };
+                  })}
+                  onChange={handleLocalChange}
+                  value={selectedType}
+                />
+              </Button>
+            }
+            {renderList()}
+          </Grid.Column>
+          <Grid.Column width={12}>
+            {boundary && <BoundaryDetailsImport
+              boundary={boundary}
+              boundaries={props.newBoundaries}
+              model={props.model}
+              difference={selectedItem ? selectedItem.diff : undefined}
+              soilmodel={props.soilmodel}
+              onChange={noHandle}
+              readOnly={true}
+            />}
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
 }
 
 export default BoundaryComparator;
