eedbd78b6ea17e1e2c906baf0e828ac8eeee0093
Robert Schlick <robert.schlick@hydwis.de>
2021-06-28 11:18:33 +0200

    Feature/2059 - T03: One head observation wit one timestep results in model error.
    * Show error message when there is only one observation point
    * Change names of observation wells: remove special characters and replace whitespaces with underscores

--- src/core/model/modflow/boundaries/ObservationPoint.type.ts
+++ src/core/model/modflow/boundaries/ObservationPoint.type.ts
@@ -2,22 +2,22 @@ import {ISpValues} from './Boundary.type';
 import {Point} from 'geojson';
 
 export interface IObservationPoint {
-    type: 'Feature';
-    id: string;
-    geometry: Point;
-    properties: {
-        name: string;
-        date_times?: string[];
-        sp_values: ISpValues;
-        type: 'op';
-        distance: number;
-    };
+  type: 'Feature';
+  id: string;
+  geometry: Point;
+  properties: {
+    name: string;
+    date_times?: string[];
+    sp_values: ISpValues;
+    type: 'op';
+    distance: number;
+  };
 }
 
 export interface IObservationPointExport {
-    id?: string;
-    name: string;
-    geometry: Point;
-    date_times?: string[];
-    sp_values: ISpValues;
+  id?: string;
+  name: string;
+  geometry: Point;
+  date_times?: string[];
+  sp_values: ISpValues;
 }
