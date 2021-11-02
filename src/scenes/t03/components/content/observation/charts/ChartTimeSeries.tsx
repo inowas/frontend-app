f5b94756daef3702f0f525ff0d2e9de7a3eabb40
Robert Schlick <robert@schlick.it>
2021-11-02 10:28:05 +0200

    Feature/2051 - Add timeseries diagram
    * Finish implementation

--- src/scenes/t03/components/content/observation/charts/ChartTimeSeries.tsx
+++ src/scenes/t03/components/content/observation/charts/ChartTimeSeries.tsx
@@ -1,18 +1,20 @@
-import { BoundaryCollection, ModflowModel } from '../../../../../../core/model/modflow';
+import { BoundaryCollection, ModflowModel, Stressperiods } from '../../../../../../core/model/modflow';
 import {
   CartesianGrid,
+  LabelFormatter,
   LabelProps,
   Legend,
+  LegendValueFormatter,
   Line,
   LineChart,
   ReferenceLine,
   ResponsiveContainer,
   Tooltip,
+  TooltipFormatter,
   XAxis,
   YAxis,
 } from 'recharts';
-import { ChangeEvent, useCallback, useEffect, useState } from 'react';
-import { DropdownProps, Form, InputProps, Segment } from 'semantic-ui-react';
+import { DropdownProps, Form, Segment } from 'semantic-ui-react';
 import { EBoundaryType } from '../../../../../../core/model/modflow/boundaries/Boundary.type';
 import { EResultType } from '../../../../../modflow/components/content/results/flowResults';
 import { HeadObservationWell } from '../../../../../../core/model/modflow/boundaries';
@@ -20,8 +22,12 @@ import { IHeadObservationWell } from '../../../../../../core/model/modflow/bound
 import { IRootReducer } from '../../../../../../reducers';
 import { MODFLOW_CALCULATION_URL, fetchApiWithToken } from '../../../../../../services/api';
 import { SyntheticEvent } from 'react-router/node_modules/@types/react';
+import { cloneDeep } from 'lodash';
+import { distinct } from '../../../../../modflow/defaults/colorScales';
 import { misc } from '../../../../defaults/colorScales';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import React from 'react';
 
 interface IData {
   sp: string;
@@ -29,67 +35,26 @@ interface IData {
 }
 
 const ChartTimeSeries = () => {
-  const [activeInput, setActiveInput] = useState<string | null>(null);
-  const [activeValue, setActiveValue] = useState<string>('');
-  const [data, setData] = useState<IData[]>([]);
+  const [data, setData] = useState<IData[] | null>(null);
   const [isFetching, setIsFetching] = useState<boolean>(false);
-  const [minMax, setMinMax] = useState<[number, number]>([0, 0]);
-  const [selectedWell, setSelectedWell] = useState<IHeadObservationWell | null>(null);
+  const [selectedWells, setSelectedWells] = useState<IHeadObservationWell[]>([]);
 
+  const user = useSelector((state: IRootReducer) => state.user);
   const T03 = useSelector((state: IRootReducer) => state.T03);
   const boundaries = T03.boundaries ? BoundaryCollection.fromObject(T03.boundaries) : null;
-
-  const fetchCellData = useCallback(async () => {
-    const model = T03.model ? ModflowModel.fromObject(T03.model) : null;
-    if (!model || !selectedWell) {
-      return;
-    }
-
-    const hob = HeadObservationWell.fromObject(selectedWell);
-
-    setIsFetching(true);
-    try {
-      const c = await fetchApiWithToken(
-        `${MODFLOW_CALCULATION_URL}/${model?.calculationId}/timeseries/types/${EResultType.HEAD}/layers/0/rows/${hob.row}/columns/${hob.column}`
-      );
-      if (c.data && Array.isArray(c.data)) {
-        const d: IData[] = [];
-        model.stressperiods.totims.forEach((totim) => {
-          const obj: IData = { sp: totim.toString() };
-          const keys = hob.getDateTimes(model.stressperiods).map((d) => model.stressperiods.totimFromDate(d));
-          const keyOfStressperiod = keys.indexOf(totim);
-          if (keyOfStressperiod > -1) {
-            obj.observed = hob.getSpValues(model.stressperiods)[keyOfStressperiod][0];
-          }
-
-          const obs = c.data.filter((row: number[]) => row[0] === totim);
-          if (obs.length > 0) {
-            obj.simulated = obs[0][1];
-          }
-
-          d.push(obj);
-        });
-
-        const observed = [...d.map((r) => r.observed)].filter((n) => typeof n === 'number') as number[];
-        const simulated = [...d.map((r) => r.simulated)].filter((n) => typeof n === 'number') as number[];
-
-        const max = Math.ceil(Math.max(...observed.concat(simulated)));
-        const min = Math.floor(Math.min(...observed.concat(simulated)));
-
-        setMinMax([min, max]);
-        setData(d);
-        return c.data;
-      }
-    } catch (e) {
-      console.log(e);
-    } finally {
-      setIsFetching(false);
-    }
-  }, [T03.model, selectedWell]);
+  const model = T03.model ? ModflowModel.fromObject(T03.model) : null;
 
   useEffect(() => {
-    fetchCellData();
-  }, [fetchCellData, selectedWell]);
+    const m = T03.model ? ModflowModel.fromObject(T03.model) : null;
+    if (m) {
+      const d: IData[] = [];
+      m.stressperiods.totims.forEach((totim) => {
+        const obj: IData = { sp: totim.toString() };
+        d.push(obj);
+      });
+      setData(d);
+    }
+  }, [T03.model]);
 
   if (!boundaries) {
     return null;
@@ -101,35 +66,98 @@ const ChartTimeSeries = () => {
     return { value: 'Head (m asl)', position: 'insideLeft', angle: -90, fill: '#4C4C4C', fontSize: '13px' };
   };
 
-  const handleBlur = () => {
-    if (activeInput === 'min') {
-      setMinMax([parseFloat(activeValue), minMax[1]]);
+  const handleChangeSelectedWell = async (e: SyntheticEvent, { value }: DropdownProps) => {
+    if (!model || !Array.isArray(value) || !data) {
+      return null;
     }
-    if (activeInput === 'max') {
-      setMinMax([minMax[0], parseFloat(activeValue)]);
-    }
-    setActiveInput(null);
-  };
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>, { name, value }: InputProps) => {
-    setActiveInput(name);
-    setActiveValue(value);
-  };
+    const filteredWells = value.filter((w) => selectedWells.filter((sw) => sw.id === w).length === 0) as string[];
 
-  const handleChangeSelectedWell = (e: SyntheticEvent, { value }: DropdownProps) => {
-    if (typeof value === 'string') {
-      const hob = boundaries.findById(value);
-      if (hob instanceof HeadObservationWell) {
-        setSelectedWell(hob.toObject());
+    if (filteredWells.length === 0) {
+      const wellToRemove = [...selectedWells].filter((sw) => value.filter((w) => w === sw.id).length === 0);
+      if (wellToRemove.length > 0) {
+        const hob = HeadObservationWell.fromObject(wellToRemove[0]);
+        setData(
+          cloneDeep(data).map((row) => {
+            delete row[`${hob.id}_observed`];
+            delete row[`${hob.id}_simulated`];
+            return row;
+          })
+        );
+        setSelectedWells([...selectedWells].filter((sw) => value.filter((w) => w === sw.id).length > 0));
       }
     }
+
+    if (filteredWells.length > 0) {
+      const hob = boundaries.findById(filteredWells[0]);
+      if (hob instanceof HeadObservationWell) {
+        setIsFetching(true);
+        const c = await fetchApiWithToken(
+          `${MODFLOW_CALCULATION_URL}/${model?.calculationId}/timeseries/types/${EResultType.HEAD}/layers/0/rows/${hob.row}/columns/${hob.column}`
+        );
+
+        const d = data.map((row) => {
+          const totim = parseFloat(row.sp);
+          const keys = hob.getDateTimes(model.stressperiods).map((d) => model.stressperiods.totimFromDate(d));
+          const keyOfStressperiod = keys.indexOf(totim);
+          const obs = c.data.filter((row: number[]) => row[0] === totim);
+
+          return {
+            ...row,
+            [`${hob.id}_observed`]:
+              keyOfStressperiod > -1 ? hob.getSpValues(model.stressperiods)[keyOfStressperiod][0] : undefined,
+            [`${hob.id}_simulated`]: obs.length > 0 ? obs[0][1] : undefined,
+          };
+        });
+
+        setData(d);
+
+        const cSelectedWells = cloneDeep(selectedWells);
+        cSelectedWells.push(hob.toObject());
+        setSelectedWells(cSelectedWells);
+      }
+      setIsFetching(false);
+    }
+  };
+
+  const legendFormatter: LegendValueFormatter = (value) => {
+    const hob = boundaries.findById(value.substr(0, value.indexOf('_')));
+    return hob ? value.replace(hob.id, hob.name) : value;
+  };
+
+  const labelFormatter: LabelFormatter = (value) => {
+    if (!model) {
+      return value;
+    }
+
+    return Stressperiods.dateTimeFromTotim(
+      model.stressperiods.startDateTime,
+      Number(value),
+      model.stressperiods.timeUnit
+    ).format(user.settings.dateFormat);
+  };
+
+  const tooltipFormatter: TooltipFormatter = (value, name, entry) => {
+    if (typeof name !== 'string') {
+      return value;
+    }
+
+    const hob = boundaries.findById(name.substr(0, name.indexOf('_')));
+
+    if (!hob) {
+      return value;
+    }
+
+    entry.name = name.replace(hob.id, hob.name);
+    return value;
   };
 
   return (
-    <Segment raised={true} loading={isFetching}>
+    <>
       <Form>
         <Form.Select
           label="Head observation well"
+          multiple
           onChange={handleChangeSelectedWell}
           options={hobs.map((hob) => {
             return {
@@ -138,45 +166,47 @@ const ChartTimeSeries = () => {
               text: hob.name,
             };
           })}
+          value={selectedWells.map((w) => w.id)}
         />
-        {data.length > 0 && (
-          <Form.Group widths="equal">
-            <Form.Input
-              fluid
-              label="Min Head"
-              name="min"
-              onBlur={handleBlur}
-              onChange={handleChange}
-              value={activeInput === 'min' ? activeValue : minMax[0]}
-              type="number"
-            />
-            <Form.Input
-              fluid
-              label="Max Head"
-              name="max"
-              onBlur={handleBlur}
-              onChange={handleChange}
-              value={activeInput === 'max' ? activeValue : minMax[1]}
-              type="number"
-            />
-          </Form.Group>
-        )}
       </Form>
-      {data.length > 0 && (
-        <ResponsiveContainer aspect={1.5}>
-          <LineChart data={data}>
-            <XAxis dataKey="sp" domain={['dataMin', 'dataMax']} />
-            <YAxis label={getYAxisLabel()} domain={minMax} />
-            <CartesianGrid strokeDasharray="3 3" />
-            <Tooltip />
-            <ReferenceLine stroke="#000" strokeDasharray="3 3" />
-            <Line type="monotone" dataKey="simulated" stroke={misc[0]} activeDot={{ r: 8 }} />
-            <Line type="monotone" dataKey="observed" stroke={misc[0]} strokeDasharray="2" activeDot={{ r: 8 }} />
-            <Legend iconType="plainline" iconSize={30} verticalAlign="middle" wrapperStyle={{ top: 0, left: 350 }} />
-          </LineChart>
-        </ResponsiveContainer>
+      {data && selectedWells.length > 0 && (
+        <Segment raised={true} loading={isFetching}>
+          <ResponsiveContainer aspect={1.5}>
+            <LineChart data={data}>
+              <XAxis dataKey="sp" />
+              <YAxis label={getYAxisLabel()} />
+              <CartesianGrid strokeDasharray="3 3" />
+              <Tooltip formatter={tooltipFormatter} labelFormatter={labelFormatter} />
+              <ReferenceLine stroke="#000" strokeDasharray="3 3" />
+              {selectedWells.map((hob, key) => (
+                <React.Fragment key={`${hob.id}_${key}`}>
+                  <Line
+                    type="monotone"
+                    dataKey={`${hob.id}_simulated`}
+                    stroke={key < distinct.length ? distinct[key] : distinct[misc.length - 1]}
+                    activeDot={{ r: 8 }}
+                  />
+                  <Line
+                    type="monotone"
+                    dataKey={`${hob.id}_observed`}
+                    stroke={key < distinct.length ? distinct[key] : distinct[misc.length - 1]}
+                    strokeDasharray="2"
+                    activeDot={{ r: 8 }}
+                  />
+                </React.Fragment>
+              ))}
+              <Legend
+                iconType="plainline"
+                iconSize={30}
+                verticalAlign="bottom"
+                wrapperStyle={{ bottom: -10, left: 0 }}
+                formatter={legendFormatter}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </Segment>
       )}
-    </Segment>
+    </>
   );
 };
 
