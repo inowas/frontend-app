fe2eebdfb7b4e66570928e9588bf9dda003b6846
Robert <robert@schlick.it>
2022-06-13 08:12:21 +0200

    Feature/2146 - Increase maximum zoom levels

--- src/services/geoTools/tileLayers.js
+++ src/services/geoTools/tileLayers.js
@@ -1,9 +1,11 @@
-import {TileLayer} from 'react-leaflet';
+import { TileLayer } from 'react-leaflet';
 import React from 'react';
 
 export const BasicTileLayer = () => (
-    <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-    />
+  <TileLayer
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    maxNativeZoom={18}
+    maxZoom={24}
+  />
 );
