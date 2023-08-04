7d9031eec416b28cb85e4fde441816332224497f
Ralf Junghanns <ralf.junghanns@gmail.com>
2023-08-04 13:12:52 +0200

    Fix bug writing to session storage (#492)
    * bugfix out of quota
    
    Co-authored-by: Ralf Junghanns <ralf@junghanns.it>

--- src/services/dataDropper/DataDropperHelper.ts
+++ src/services/dataDropper/DataDropperHelper.ts
@@ -1,47 +1,66 @@
 import { DATADROPPER_URL } from '../api';
 import { IDataDropperFile } from './DataDropper.type';
 
-export async function dropData(data: any) {
-    const response = await fetch(
-        DATADROPPER_URL, {
-            method: 'post',
-            body: JSON.stringify(data),
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json'
-            }
-        });
+const readFromSessionStorage = (filename: string) => {
+  const sessionStorageObj = sessionStorage.getItem(filename);
+  if (sessionStorageObj) {
+    return JSON.parse(sessionStorageObj);
+  }
+  return null;
+};
 
-    const filename = (await response.json()).filename;
+const writeToSessionStorage = (filename: string, data: any) => {
+  try {
     sessionStorage.setItem(filename, JSON.stringify(data));
+  } catch (e) {
+    if (e instanceof DOMException) {
+      sessionStorage.clear();
+      sessionStorage.setItem(filename, JSON.stringify(data));
+    }
+  }
+};
 
-    return {
-        filename,
-        server: DATADROPPER_URL
-    } as IDataDropperFile;
+export async function dropData(data: any) {
+  const response = await fetch(
+    DATADROPPER_URL, {
+      method: 'post',
+      body: JSON.stringify(data),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+    });
+
+  const filename = (await response.json()).filename;
+  writeToSessionStorage(filename, data);
+
+  return {
+    filename,
+    server: DATADROPPER_URL,
+  } as IDataDropperFile;
 }
 
 export async function retrieveData(file: IDataDropperFile) {
-    const url = new URL(`${file.server}/${file.filename}`);
-    const localStorageObj = sessionStorage.getItem(file.filename);
-    if (localStorageObj) {
-        return JSON.parse(localStorageObj);
-    }
+  const sessionStorageObj = readFromSessionStorage(file.filename);
+  if (sessionStorageObj) {
+    return sessionStorageObj;
+  }
 
-    const response = await fetch(
-        url.toString(), {
-            method: 'get',
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json'
-            }
-        });
+  const url = new URL(`${file.server}/${file.filename}`);
+  const response = await fetch(
+    url.toString(), {
+      method: 'get',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+    });
 
-    try {
-        const data = await response.json();
-        sessionStorage.setItem(file.filename, JSON.stringify(data));
-        return data;
-    } catch (e) {
-        throw new Error('Datadropper file not found');
-    }
+  try {
+    const data = await response.json();
+    writeToSessionStorage(file.filename, data);
+    return data;
+  } catch (e) {
+    throw new Error('Datadropper file not found');
+  }
 }
